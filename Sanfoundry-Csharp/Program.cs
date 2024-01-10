a7c9975ae1e97c99e5e1d2579f1a7887968bc80f
ebenofere <ebenezerofere@gmail.com>
2024-01-10 09:02:12 -0500

    Add project files.

---
+++ Sanfoundry-Csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿//C# Program to Check whether the Entered Number is Even or Odd
+
+int i;
+Console.Write("Enter a number : ");
+i = int.Parse(Console.ReadLine());
+if (i % 2 == 0)
+{
+    Console.Write("the entered number is an even number");
+    Console.Read();
+}
+else
+{
+    Console.Write("the entered number is an odd number");
+    Console.Read();
+}
+
+
+
+
+//C# Program to Check whether the Entered Number is Even or Odd
+
+
